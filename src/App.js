c06884e8aa305f2925aadfc862d38ffb8ef2e36f
Leo Pham <hongthaipro@gmail.com>
2020-10-15 09:42:38 +0700

    aa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Layout, Menu, PageHeader } from "antd";
+import {
+  CloudDownloadOutlined,
+  HistoryOutlined,
+  SettingOutlined,
+  LoginOutlined,
+  MessageOutlined,
+  InfoCircleOutlined,
+  ToolOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
 
-function App() {
+const { Sider, Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider trigger={null}>
+        {/* <div className="logo" /> */}
+        <Menu theme="light" mode="inline" defaultSelectedKeys={["download"]}>
+          <Menu.ItemGroup title="Tool" icon={<ToolOutlined />}>
+            <Menu.Item key="download" icon={<CloudDownloadOutlined />}>
+              Download
+            </Menu.Item>
+            <Menu.Item key="history" icon={<HistoryOutlined />}>
+              History
+            </Menu.Item>
+            <Menu.Item key="setting" icon={<SettingOutlined />}>
+              Settings
+            </Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Others" icon={<TeamOutlined />}>
+            <Menu.Item key="login" icon={<LoginOutlined />}>
+              Login
+            </Menu.Item>
+            <Menu.Item key="contact" icon={<MessageOutlined />}>
+              Contact
+            </Menu.Item>
+            <Menu.Item key="about" icon={<InfoCircleOutlined />}>
+              About
+            </Menu.Item>
+          </Menu.ItemGroup>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <PageHeader
+          ghost={false}
+          subTitle='Copy the url, paste to below, click "start" button, see the magic!'
+        />
+        <Content className="site-layout-background"></Content>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
